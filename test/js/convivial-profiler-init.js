e31c01c25b8b97104b37c1f47ac9ab17755790b1
Eleo Basili <eleonel.basili@gmail.com>
2024-02-22 13:56:04 -0300

    Add dynamic form creation on testing tab.

--- test/js/convivial-profiler-init.js
+++ test/js/convivial-profiler-init.js
@@ -1,7 +1,52 @@
 // convivial-profiler-init.js
 (function (window, ConvivialProfiler) {
+  document.addEventListener('DOMContentLoaded', function () {
+    const jsonData = JSON.parse(localStorage.getItem('profilersData')) || {};
+    console.log(jsonData);
+    const tbody = document.querySelector('#profilersTable tbody');
+  
+    Object.entries(jsonData.config.profilers).forEach(([name, profiler], index) => {
+        const row = tbody.insertRow();
+        row.innerHTML = `
+            <th scope="row">
+              <h5>Name: ${name}</h5>
+              <button type="button" class="btn btn-success execute-profiler mt-2 btn-sm disabled">Execute</button>
+              <textarea id="testingProfiler" class="form-control mt-4" style="height: 600px;"></textarea>
+
+            </th>
+            <td>${generateAccordion(profiler.sources, 'sources-' + index)}</td>
+            <td>${generateAccordion(profiler.processors, 'processors-' + index)}</td>
+            <td>${generateAccordion(profiler.destinations, 'destinations-' + index)}</td>
+        `;
+    });
+  });
+  
+  function generateAccordion(items, parentId) {
+    let accordion = `<div class="accordion" id="${parentId}">`;
+    items.forEach((item, idx) => {
+        const collapseId = `collapse${parentId}${idx}`;
+        accordion += `
+            <div class="accordion-item">
+                <h5 class="accordion-header" id="heading${collapseId}">${item.type}</h5>
+                <div id="${collapseId}">
+                    <div class="accordion-body">
+                      ${Object.entries(item).filter(([key]) => key !== 'type').map(([key, value]) => 
+                      `<div class="mb-3">
+                          <label class="form-label">${value} (${key})</label>
+                          <input type="text" class="form-control" name="${key}" value="${value}">
+                      </div>`
+                ).join('')}
+                    </div>
+                </div>
+            </div>
+        `;
+    });
+    accordion += `</div>`;
+    return accordion;
+  }
+  
   window.testBuilder.onConfigReady = function () {
     window.convivialProfiler = new ConvivialProfiler(window.testBuilder.convivialProfiler.config, window.testBuilder.convivialProfiler.site);
-    window.convivialProfiler.collect();
+    //window.convivialProfiler.collect();
   };
 })(window, window.ConvivialProfiler.default);
--- test/js/profiler.js
+++ test/js/profiler.js
@@ -7,6 +7,7 @@ window.testBuilder = {
 // Listening for reply from parent
 window.addEventListener('message', function (event) {
   window.testBuilder.convivialProfiler = JSON.parse(event.data.value || '{}');
+  localStorage.setItem('profilersData', event.data.value);
   if (typeof window.testBuilder.onConfigReady === 'function') {
     window.testBuilder.onConfigReady();
   }
